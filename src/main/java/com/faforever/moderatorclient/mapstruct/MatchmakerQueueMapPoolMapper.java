aa09ba79cd2146481b64dbcae1f761b627ef2248
SKeeneCode <samkeene@protonmail.com>
2020-10-06 23:09:34 +0100

    add mappers

---
+++ src/main/java/com/faforever/moderatorclient/mapstruct/MatchmakerQueueMapPoolMapper.java
@@ -0,0 +1,16 @@
+package com.faforever.moderatorclient.mapstruct;
+
+import com.faforever.moderatorclient.common.MatchmakerQueueMapPool;
+import com.faforever.moderatorclient.ui.domain.MatchmakerQueueMapPoolFX;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {JavaFXMapper.class, MatchmakerQueueMapper.class, MapPoolMapper.class})
+public abstract class MatchmakerQueueMapPoolMapper {
+
+    public abstract MatchmakerQueueMapPoolFX mapToFx(MatchmakerQueueMapPool dto);
+    public abstract MatchmakerQueueMapPool mapToDto(MatchmakerQueueMapPoolFX fxBean);
+    public abstract List<MatchmakerQueueMapPoolFX> mapToFx(List<MatchmakerQueueMapPool> dtoList);
+    public abstract List<MatchmakerQueueMapPool> mapToDto(List<MatchmakerQueueMapPoolFX> fxBeanList);
+}
