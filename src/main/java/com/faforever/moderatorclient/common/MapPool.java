d0439503eafe69d7145d260d7e4afbccbbce7168
SKeeneCode <samkeene@protonmail.com>
2020-10-13 02:10:51 +0100

    rework add/removing maps from brackets

--- src/main/java/com/faforever/moderatorclient/common/MapPool.java
+++ src/main/java/com/faforever/moderatorclient/common/MapPool.java
@@ -6,13 +6,11 @@ import com.github.jasminb.jsonapi.annotations.Relationship;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
-@ToString
 @Type("mapPool")
 public class MapPool extends AbstractEntity {
 
@@ -20,4 +18,5 @@ public class MapPool extends AbstractEntity {
 
     @Relationship("mapVersions")
     private List<MapVersion> mapVersions;
+
 }
