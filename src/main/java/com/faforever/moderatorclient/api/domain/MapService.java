752a3e0672a88d2d3a4cc7cda8a0c93f9f7e6b69
Sheikah45 <Sheikah450@gmail.com>
2021-06-10 22:21:54 -0400

    Use Hydra OAuth for logging in to the api

--- src/main/java/com/faforever/moderatorclient/api/domain/MapService.java
+++ src/main/java/com/faforever/moderatorclient/api/domain/MapService.java
@@ -41,9 +41,9 @@ public class MapService {
     private final MapPoolAssignmentMapper mapPoolAssignmentMapper;
     private final MatchmakerQueueMapPoolMapper matchmakerQueueMapPoolMapper;
 
-    @Value("${faforever.map-generator.queryVersionsUrl}")
+    @Value("${faforever.map-generator.query-versions-url}")
     private String generatorVersionsURL;
-    @Value("${faforever.map-generator.minSupportedVersion}")
+    @Value("${faforever.map-generator.min-supported-version}")
     private String minGeneratorVersion;
 
     private List<Map> findMapsByAttribute(@NotNull String attribute, @NotNull String pattern, boolean excludeHidden) {
