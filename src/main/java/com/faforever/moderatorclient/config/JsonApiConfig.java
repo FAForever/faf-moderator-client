6d9b08d6989c7091c35abb1880d026bc1ee4752c
Brutus5000 <brutus5000@gmx.net>
2021-09-29 22:40:32 +0200

    Remove noCatch dependency and jcenter repository

--- src/main/java/com/faforever/moderatorclient/config/JsonApiConfig.java
+++ src/main/java/com/faforever/moderatorclient/config/JsonApiConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jasminb.jsonapi.ResourceConverter;
 import com.github.jasminb.jsonapi.annotations.Type;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +14,6 @@ import org.springframework.core.type.filter.AnnotationTypeFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.github.nocatch.NoCatch.noCatch;
 import static java.lang.Class.forName;
 
 @Configuration
@@ -36,9 +37,14 @@ public class JsonApiConfig {
             ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
             provider.addIncludeFilter(new AnnotationTypeFilter(Type.class));
             provider.findCandidateComponents(packageName).stream()
-                    .map(beanDefinition -> noCatch(() -> (Class) forName(beanDefinition.getBeanClassName())))
+                    .map(this::resolveClass)
                     .forEach(classes::add);
         }
         return classes.toArray(new Class<?>[classes.size()]);
+    }
+
+    @SneakyThrows
+    private Class<?> resolveClass(BeanDefinition beanDefinition) {
+        return forName(beanDefinition.getBeanClassName());
     }
 }
