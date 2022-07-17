081ab86e8f4c3cb4ad366e2fb1ae28d85150f474
Brutus5000 <brutus5000@gmx.net>
2022-07-17 22:24:45 +0200

    Fixes #169 Revoking of tokens (affects ban creation)

--- src/main/java/com/faforever/moderatorclient/api/FafUserCommunicationService.java
+++ src/main/java/com/faforever/moderatorclient/api/FafUserCommunicationService.java
@@ -8,15 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 @Service
@@ -52,21 +47,8 @@ public class FafUserCommunicationService {
     public void authorize(HydraAuthorizedEvent event) {
         restTemplate = restTemplateBuilder
                 .rootUri(environmentProperties.getUserBaseUrl())
-                .interceptors(List.of(oAuthTokenInterceptor,
-                (request, body, execution) -> {
-                    HttpHeaders headers = request.getHeaders();
-
-                    List<String> contentTypes = headers.get(HttpHeaders.CONTENT_TYPE);
-                    if (contentTypes != null && contentTypes.stream()
-                            .anyMatch(MediaType.APPLICATION_JSON_VALUE::equalsIgnoreCase)) {
-                        headers.setAccept(Collections.singletonList(MediaType.valueOf("application/vnd.api+json")));
-                        if (request.getMethod() == HttpMethod.POST || request.getMethod() == HttpMethod.PATCH || request.getMethod() == HttpMethod.PUT) {
-                            headers.setContentType(MediaType.APPLICATION_JSON);
-                        }
-                    }
-                    return execution.execute(request, body);
-                }
-        )).build();
+                .interceptors(oAuthTokenInterceptor)
+                .build();
         authorizedLatch.countDown();
     }
 
